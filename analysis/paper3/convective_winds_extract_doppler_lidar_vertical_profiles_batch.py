c2ad91e0d9c7ae6668e51a021b4a96ae24643fd0
elischwat <eli.l.schwat@gmail.com>
2024-04-17 15:09:06 -0700

    more analysis for paper3

--- analysis/paper3/convective_winds_extract_doppler_lidar_vertical_profiles_batch.py
+++ analysis/paper3/convective_winds_extract_doppler_lidar_vertical_profiles_batch.py
@@ -1,69 +1,78 @@
-"""_summary_
+"""This script aggregates doppler lidar data and saves out files. 
+It downloads (or uses already downloaded) SAIL doppler lidar data, 
+isolates along-valley RHI scan data and, for each scan:
+1. aggregates the scan data in vertical and horizontal bins,
+2. calculates mean, median, std dev, and count for each bin,
+3. saves the data to parquet file.
 """
+
 import numpy as np
 import pandas as pd
 import act
 import os
 import datetime as dt
-import datetime as dt
-import altair as alt
-alt.data_transformers.enable('json')
+from joblib import Parallel, delayed
+from tqdm import tqdm
+import glob
+import warnings
+import pandas as pd
+from pandas.errors import SettingWithCopyWarning
+warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
 
-
-USERNAME = os.getenv("ARM_USERNAME")
-TOKEN = os.getenv("ARM_TOKEN")
+# USER INPUTS
+# Download files from SAIL servers. If false, looks in provided directories
+DOWNLOAD = False
+# don't change this one
 SAIL_DATA_STREAM = 'gucdlrhiM1.b1'
-sail_download_path = f"/Users/elischwat/Development/data/sublimationofsnow/{SAIL_DATA_STREAM}/"
-processed_output_path = f"/Users/elischwat/Development/data/sublimationofsnow/sail_processed/{SAIL_DATA_STREAM}/"
-PARALLELISM = 8
+sail_download_path = f"/storage/elilouis/sublimationofsnow/{SAIL_DATA_STREAM}/"
+processed_output_path = f"/storage/elilouis/sublimationofsnow/sail_processed/{SAIL_DATA_STREAM}/"
+PARALLELISM = 16 # 10 seems optimal on j-lunquist-3; 20 overloaded the memory
 VALLEY_INCLINE = 10
 OFFSETS = [-1500, -1000, -500, -250, -125, 125, 250, 500, 1000, 1500]
 SAMPLING_TOLERANCE = 50
 SNR_THRESHOLD = 0.008
-DATE_FORMAT = "%Y-%m-%d"
 MAX_RANGE = 2000
-import multiprocessing
-from joblib import Parallel, delayed
-from tqdm import tqdm
+START_DATE = "20230201"
+END_DATE = "20230616"
 
-dates = [
-    dt.date(2023, 3, 3), # actual date of interest
-    dt.date(2023, 3, 4), 
-    dt.date(2023, 4, 16), # actual date of interest
-    dt.date(2023, 4, 17),
-    dt.date(2023, 5, 2), # actual date of interest
-    dt.date(2023, 5, 3),
-    # # dt.date(2023, 6, 9),  # actual date of interest
-    # # dt.date(2023, 6, 10),
-    # # dt.date(2023, 6, 11),  # actual date of interest
-    # # dt.date(2023, 6, 12), 
-]
+USERNAME = os.getenv("ARM_USERNAME")
+TOKEN = os.getenv("ARM_TOKEN")
 
+DATE_FORMAT = "%Y-%m-%d"
+datelist = [timestamp.date() for timestamp in pd.date_range(START_DATE, END_DATE).tolist()]
 
-
-# +
-print("Beginning data download...")
 def process_date(date):
-    files = act.discovery.download_arm_data(
-        USERNAME, 
-        TOKEN, 
-        SAIL_DATA_STREAM, 
-        date.strftime('%Y%m%d'), 
-        date.strftime('%Y%m%d'), 
-        output=sail_download_path
-    )
+    print(f"Processing {date}...")
+    if DOWNLOAD:
+        print("\tBeginning data download...")
+        files = act.discovery.download_arm_data(
+            USERNAME, 
+            TOKEN, 
+            SAIL_DATA_STREAM, 
+            date.strftime('%Y%m%d'), 
+            date.strftime('%Y%m%d'), 
+            output=sail_download_path
+        )
 
-    print("Data download complete.")
+        print("\tData download complete.")
+        print("\tOpening files...")
+        dl_rhi = act.io.arm.read_arm_netcdf(files)
+        print("\tFile opening complete.")
+    else:
+        print("\tSkipping data download... opening files locally")
+        files = glob.glob(os.path.join(
+            sail_download_path,
+            f"{SAIL_DATA_STREAM}.{date.strftime('%Y%m%d')}.*.cdf"
+        ))
+        print(f"\tOpening files...{len(files)} of them")
+        dl_rhi = act.io.arm.read_arm_netcdf(files)
+        print("\tFile opening complete.")
 
-    print("Opening files...")
-    dl_rhi = act.io.arm.read_arm_netcdf(files)
-    print("File opening complete.")
-
-    print("Converting to dataframe...")
+    print("\tConverting to dataframe...")
     src_rhi = dl_rhi.to_dataframe().reset_index()
-    print("Conversion complete.")
+    print("\tConversion complete.")
 
-    print("Preprocessing data")
+    print("\tPreprocessing data")
     # Convert time zone
     src_rhi['time'] = src_rhi['time'].dt.tz_localize('UTC').dt.tz_convert('US/Mountain')
     src_rhi['time'] = pd.to_datetime(src_rhi['time'].dt.tz_localize(None))
@@ -91,9 +100,9 @@ def process_date(date):
     # Velocities are still positive down-valley.
     
     src_rhi['streamwise_velocity'] = src_rhi['horizontal_velocity'] / np.cos(np.deg2rad(VALLEY_INCLINE))
-    print("Preprocessing complete")
+    print("\tPreprocessing complete")
     
-    print("Isolating along-valley scans")
+    print("\tIsolating along-valley scans")
     # Separate cross valley and along valley scans
     ############################################################################################################
     ############################################################################################################
@@ -102,61 +111,80 @@ def process_date(date):
     # Isolate valley-wise RHI scans
     valley_rhi_df = src_rhi[np.abs(src_rhi['azimuth'] - scan_azimuth_valley_wise) < 1]
 
-    # Label the 4 valley-wise scans that happen each hour
-    # similarly to above
-    valley_rhi_df['hourly_seconds'] = valley_rhi_df.apply(lambda row: row['minute']*60 + row['second'], axis=1)
-    valley_rhi_df['hourly_scan_n'] = pd.cut(
-        valley_rhi_df['hourly_seconds'],
-        [22, 214, 1804, 2014, 3599],
-        labels=['00.00','03.00','30.00','33.00']
-    )
+    if len(valley_rhi_df) == 0:
+        print(f"\tFound no along=valley RHI scans for date {date}")
+        return None
+    else:
+        # Label the 4 valley-wise scans that happen each hour
+        # similarly to above
+        valley_rhi_df['hourly_seconds'] = valley_rhi_df.apply(lambda row: row['minute']*60 + row['second'], axis=1)
+        valley_rhi_df['hourly_scan_n'] = pd.cut(
+            valley_rhi_df['hourly_seconds'],
+            [22, 214, 1804, 2014, 3599],
+            labels=['00.00','03.00','30.00','33.00']
+        )
+        print(f"\t{len(valley_rhi_df)}")
+        valley_rhi_df = valley_rhi_df.dropna(subset=['hourly_scan_n'])
+        print(f"\t{len(valley_rhi_df)}")
 
-    valley_rhi_df.loc[:, 'scan_time'] = valley_rhi_df.apply(lambda row : row['time_beginning_of_hour']+ dt.timedelta(minutes = float(row['hourly_scan_n'])), axis=1)
-    valley_rhi_df['scan_time'] = valley_rhi_df.apply(
-        lambda row: row['scan_time'].replace(
-            year = row['time'].year, 
-            month = row['time'].month, 
-            day = row['time'].day,
-        ),
-        axis=1
-    )
-    print("Isolating along-valley scans complete")
-    
-    print("Extracting profiles")
-    ############################################################################################################
-    ############################################################################################################
-    # bin z so we have high res near surface (<100m), low res above
-    z_bins = np.concatenate([
-        np.linspace(0,100,11), 
-        np.linspace(0,2000,41)[3:]
-    ])
-
-    valley_rhi_df['z_binned'] = pd.cut(valley_rhi_df['z'], bins=z_bins).apply(lambda bin: (bin.left + bin.right)/2).astype('float')
-
-    df_list = []
-    
-    for x_offset in OFFSETS:
-        this_offset_df = valley_rhi_df[np.abs(valley_rhi_df['x'] - x_offset) < SAMPLING_TOLERANCE]
-        this_offset_df = this_offset_df.assign(x_offset = x_offset)
-        df_list.append(this_offset_df)
+        valley_rhi_df['scan_time'] = valley_rhi_df.apply(
+            lambda row : row['time_beginning_of_hour']+ dt.timedelta(minutes = float(row['hourly_scan_n'])),
+            axis=1
+        )
+        valley_rhi_df['scan_time'] = valley_rhi_df.apply(
+            lambda row: row['scan_time'].replace(
+                year = row['time'].year, 
+                month = row['time'].month, 
+                day = row['time'].day,
+            ),
+            axis=1
+        )
+        print("\tIsolating along-valley scans complete")
         
-    spatial_src_df = pd.concat(df_list)
+        print("\tExtracting profiles")
+        ############################################################################################################
+        ############################################################################################################
+        # bin z so we have high res near surface (<100m), low res above
+        z_bins = np.concatenate([
+            np.linspace(0,100,11), 
+            np.linspace(0,2000,41)[3:]
+        ])
 
-    spatial_src_df = spatial_src_df.groupby(['z_binned', 'x_offset', 'scan_time'])[['horizontal_velocity', 'radial_velocity', 'streamwise_velocity']].median().reset_index()
+        valley_rhi_df['z_binned'] = pd.cut(valley_rhi_df['z'], bins=z_bins).apply(lambda bin: (bin.left + bin.right)/2).astype('float')
 
-    print("Saving to file")
-    output_file = os.path.join(processed_output_path, str(date) + '.parquet')
-    spatial_src_df.to_parquet(
-        output_file
-    )
-    return output_file
+        df_list = []
+        
+        for x_offset in OFFSETS:
+            this_offset_df = valley_rhi_df[np.abs(valley_rhi_df['x'] - x_offset) < SAMPLING_TOLERANCE]
+            this_offset_df = this_offset_df.assign(x_offset = x_offset)
+            df_list.append(this_offset_df)
+            
+        spatial_src_df = pd.concat(df_list)
+
+        spatial_src_df = spatial_src_df.groupby(
+            ['z_binned', 'x_offset', 'scan_time']
+        )[
+            ['horizontal_velocity', 'radial_velocity', 'streamwise_velocity']
+        ].agg(['mean', 'median', 'std', 'count']).reset_index()
+
+        print("\tSaving to file")
+        output_file = os.path.join(processed_output_path, str(date) + '.parquet')
+        spatial_src_df.to_parquet(
+            output_file
+        )
+        return output_file
     
 if __name__ == '__main__':
-    dates_tqdm = tqdm(dates)
+    dates_tqdm = tqdm(datelist)
 
     print(f"Beginning processing (parallelism = {PARALLELISM})")
+    # slow way
+    # processed_results = [process_date(date) for date in dates_tqdm]
+    # fast way
     processed_results =  Parallel(n_jobs = PARALLELISM)(
         delayed(process_date)(date) for date in dates_tqdm
     )
+    # Remove Nones
+    processed_results = [p for p in processed_results if p]
     print(f"Finished processing, {len(processed_results)} files generated.")
     
