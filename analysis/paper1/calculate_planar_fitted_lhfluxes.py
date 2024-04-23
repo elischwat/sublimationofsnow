42d3f21fe5e5608480c34b349647dd3a74b45a8a
elischwat <eli.l.schwat@gmail.com>
2024-04-22 17:53:47 -0700

    updated

---
+++ analysis/paper1/calculate_planar_fitted_lhfluxes.py
@@ -0,0 +1,173 @@
+# ---
+
+# ---
+import numpy as np
+import os
+import xarray as xr
+import datetime as dt
+import pandas as pd
+import matplotlib.pyplot as plt
+import altair as alt
+alt.data_transformers.enable('json')
+from sublimpy import utils, extrautils
+import glob
+from joblib import Parallel, delayed
+from tqdm import tqdm
+
+# Save to data path
+OUTPUT_PATH = '/Users/elischwat/Development/data/sublimationofsnow/planar_fit/'
+
+# # Open fast data
+file_list = sorted(glob.glob("/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast/*.nc"))
+file_list = [f for f in file_list if '202210' not in f]
+
+# Open planar fit fit data
+monthly_file = "/Users/elischwat/Development/data/sublimationofsnow/monthly_planar_fits.csv"
+weekly_file = "/Users/elischwat/Development/data/sublimationofsnow/weekly_planar_fits.csv"
+fits_df = pd.read_csv(monthly_file, delim_whitespace=True)
+weeklyfits_df = pd.read_csv(weekly_file, delim_whitespace=True)
+fits_df['height'] = fits_df['height'].str.replace('_', '.').astype('float')
+weeklyfits_df['start_date'] = pd.to_datetime(weeklyfits_df['start_date'], format='%Y%m%d')
+weeklyfits_df['end_date'] = pd.to_datetime(weeklyfits_df['end_date'], format='%Y%m%d')
+fits_df['W_f'] = fits_df.apply(
+    lambda row: [row['W_f_1'], row['W_f_2'], row['W_f_3']],
+    axis=1
+).drop(columns=['W_f_1', 'W_f_2', 'W_f_3'])
+weeklyfits_df['W_f'] = weeklyfits_df.apply(
+    lambda row: [row['W_f_1'], row['W_f_2'], row['W_f_3']],
+    axis=1
+).drop(columns=['W_f_1', 'W_f_2', 'W_f_3'])
+
+def process_files(file_list, output_file):
+
+    ds = xr.open_mfdataset(file_list, concat_dim="time", combine="nested")
+
+    ds = ds[[
+        'base_time',
+        'u_3m_c',	'v_3m_c',	'w_3m_c',	'h2o_3m_c',		'tc_3m_c',
+        'u_3m_d',	'v_3m_d',	'w_3m_d',	'h2o_3m_d',		'tc_3m_d',
+        'u_3m_ue',	'v_3m_ue',	'w_3m_ue',	'h2o_3m_ue',	'tc_3m_ue',
+        'u_3m_uw',	'v_3m_uw',	'w_3m_uw',	'h2o_3m_uw',	'tc_3m_uw',
+        'u_5m_c',	'v_5m_c',	'w_5m_c',	'h2o_5m_c',		'tc_5m_c',
+        'u_10m_c',	'v_10m_c',	'w_10m_c',	'h2o_10m_c',	'tc_10m_c',
+        'u_10m_d',	'v_10m_d',	'w_10m_d',	'h2o_10m_d',	'tc_10m_d',
+        'u_10m_ue',	'v_10m_ue',	'w_10m_ue',	'h2o_10m_ue',	'tc_10m_ue',
+        'u_10m_uw',	'v_10m_uw',	'w_10m_uw',	'h2o_10m_uw',	'tc_10m_uw',
+        'u_15m_c',	'v_15m_c',	'w_15m_c',	'h2o_15m_c',	'tc_15m_c',
+        'u_20m_c',	'v_20m_c',	'w_20m_c',	'h2o_20m_c',	'tc_20m_c',
+    ]]
+
+    # # Create timestamp
+    # To use the datam, its necessary to combine 3 columns of data from the dataset to get the full timestamp. This is demonstrated below. The 'time' column actually only incudes the second and minute information. For all datapoints, the hour according to the 'time' column is 1.  The 'base_time' column indicates the hour of the day. The 'sample' column indicates the 20hz sample number. 
+    df = ds.to_dataframe().reset_index()
+
+    df['time'] = df.apply(lambda row: dt.datetime(
+            year = row['time'].year,
+            month = row['time'].month,
+            day = row['time'].day,
+            hour = row['base_time'].hour,
+            minute = row['time'].minute,
+            second = row['time'].second,
+            microsecond = int(row['sample'] * (1e6/20))
+        ),
+        axis = 1
+    )
+    ds = df.set_index('time').to_xarray()
+
+    # # Define function to do Reynolds Averaging
+    def create_re_avg_ds(
+            ds, 
+            re_avg_period_size, 
+            var1,
+            var2,
+            covariance_name
+    ):
+        coarse_ds = ds.coarsen(time=re_avg_period_size).mean()
+        coarse_ds = coarse_ds.assign_coords(time = coarse_ds.time.dt.round('1s'))
+        coarse_ds = coarse_ds.reindex_like(ds, method='nearest')
+        ds[f"{var1}_mean"] = coarse_ds[f"{var1}"]
+        ds[f"{var1}_fluc"] = ds[f"{var1}"] - ds[f"{var1}_mean"]
+        ds[f"{var2}_mean"] = coarse_ds[f"{var2}"]
+        ds[f"{var2}_fluc"] = ds[f"{var2}"] - ds[f"{var2}_mean"]
+        ds[covariance_name] = ds[f"{var2}_fluc"] * ds[f"{var1}_fluc"]
+        ds = ds.coarsen(time = re_avg_period_size).mean()
+        ds = ds.assign_coords(time = ds.time.dt.round('1s'))
+        return ds
+
+
+    # # Iterate over variables, apply planar fit to fast data, and calculate covariance fluxes
+    MONTH = ds.time.dt.month.values[0]
+
+    df_list = []
+    for tower in ['c', 'uw', 'ue', 'd']:
+        if tower == 'c':
+            heights = [3,5,10,15,20]
+        else:
+            heights = [3,10]
+        
+        for height in heights:
+            fitting_params = fits_df.set_index(['month', 'height', 'tower']).loc[
+                MONTH,
+                height,
+                tower
+            ]
+            u, v, w = extrautils.apply_planar_fit(
+                ds[f'u_{height}m_{tower}'].values.flatten(),
+                ds[f'v_{height}m_{tower}'].values.flatten(),
+                ds[f'w_{height}m_{tower}'].values.flatten(),
+                fitting_params['a'], 
+                fitting_params['W_f'],
+            )
+            ds[f'u_{height}m_{tower}_fit'] = ('time', u)
+            ds[f'v_{height}m_{tower}_fit'] = ('time', v)
+            ds[f'w_{height}m_{tower}_fit'] = ('time', w)
+            
+            ds_plain =  create_re_avg_ds(
+                ds, 
+                300*20, 
+                var1 = f'w_{height}m_{tower}', 
+                var2= f'h2o_{height}m_{tower}', 
+                covariance_name = f'w_h2o__{height}m_{tower}'
+            )
+            ds_fit =    create_re_avg_ds(
+                ds, 
+                300*20, 
+                var1 = f'w_{height}m_{tower}_fit', 
+                var2= f'h2o_{height}m_{tower}', 
+                covariance_name = f'w_h2o__{height}m_{tower}_fit'
+            )
+
+            merged_df = ds_plain[f'w_h2o__{height}m_{tower}'].to_dataframe()[[f'w_h2o__{height}m_{tower}']].join(
+                    ds_fit[f'w_h2o__{height}m_{tower}_fit'].to_dataframe()[[f'w_h2o__{height}m_{tower}_fit']]
+            )
+            df_list.append(merged_df)
+    
+    combined_df = df_list[0].join(df_list[1:])
+    combined_df.to_parquet(output_file)
+    return output_file
+
+
+if __name__ == '__main__':
+    n_days = int(len(file_list)/24)
+
+    def print_and_process(i):
+        start_i = i*24
+        end_i = (i+1)*24
+        output_file = os.path.join(OUTPUT_PATH, file_list[0].split('/')[-1][27:-6]) + '.parquet'
+        print(f"Processing files from {file_list[start_i].split('/')[-1]} through {file_list[end_i].split('/')[-1]} and saving to: {output_file}")
+        process_files(
+            file_list[start_i: end_i], 
+            output_file
+        )
+    #slow 
+    # saved_files_list = []
+    # for i in list(range(0, n_days)):
+    #     returned = print_and_process(i)
+    #     saved_files_list.append(returned)
+
+    # fast
+    saved_files_list =  Parallel(n_jobs = 8)(
+        delayed(print_and_process)(i) 
+        for i in tqdm(list(range(0, n_days)))
+    )
+    print("Finished processing")
--- analysis/sos/planar_fit_weekly.py
+++ analysis/sos/planar_fit_weekly.py
@@ -14,32 +14,32 @@ import datetime, calendar
 from sublimpy import utils
 
 sos_files_dir = '/Users/elischwat/Development/data/sublimationofsnow/sosqc/sos_qc_geo_tiltcor_v20240307/'
-OUTPUT_FILE = '/Users/elischwat/Development/data/sublimationofsnow/monthly_planar_fits.csv'
+OUTPUT_FILE = '/Users/elischwat/Development/data/sublimationofsnow/weekly_planar_fits.csv'
 
 
 with open(OUTPUT_FILE, "w") as file:
     variable_sets = [
-        ('u_1m_c', 'v_1m_c', 'w_1m_c'),
-        ('u_2m_c', 'v_2m_c', 'w_2m_c'),
+        # ('u_1m_c', 'v_1m_c', 'w_1m_c'),
+        # ('u_2m_c', 'v_2m_c', 'w_2m_c'),
         ('u_3m_c',   'v_3m_c',   'w_3m_c'),
         ('u_5m_c',   'v_5m_c',   'w_5m_c'),
         ('u_10m_c',   'v_10m_c',   'w_10m_c'),
         ('u_15m_c',   'v_15m_c',   'w_15m_c'),
         ('u_20m_c',   'v_20m_c',   'w_20m_c'),
         
-        ('u_1m_uw', 'v_1m_uw', 'w_1m_uw'),
-        ('u_2m_uw', 'v_2m_uw', 'w_2m_uw'),
-        ('u_2_5m_uw', 'v_2_5m_uw', 'w_2_5m_uw'),
+        # ('u_1m_uw', 'v_1m_uw', 'w_1m_uw'),
+        # ('u_2m_uw', 'v_2m_uw', 'w_2m_uw'),
+        # ('u_2_5m_uw', 'v_2_5m_uw', 'w_2_5m_uw'),
         ('u_3m_uw',   'v_3m_uw',   'w_3m_uw'),
         ('u_10m_uw',   'v_10m_uw',   'w_10m_uw'),
         
-        ('u_1m_ue', 'v_1m_ue', 'w_1m_ue'),
-        ('u_2m_ue', 'v_2m_ue', 'w_2m_ue'),
+        # ('u_1m_ue', 'v_1m_ue', 'w_1m_ue'),
+        # ('u_2m_ue', 'v_2m_ue', 'w_2m_ue'),
         ('u_3m_ue',   'v_3m_ue',   'w_3m_ue'),
         ('u_10m_ue',   'v_10m_ue',   'w_10m_ue'),
 
-        ('u_1m_d', 'v_1m_d', 'w_1m_d'),
-        ('u_2m_d', 'v_2m_d', 'w_2m_d'),
+        # ('u_1m_d', 'v_1m_d', 'w_1m_d'),
+        # ('u_2m_d', 'v_2m_d', 'w_2m_d'),
         ('u_3m_d',   'v_3m_d',   'w_3m_d'),
         ('u_10m_d',   'v_10m_d',   'w_10m_d'),
     ]
@@ -47,29 +47,34 @@ with open(OUTPUT_FILE, "w") as file:
 
     DATE_FORMAT_STR = '%Y%m%d'
 
-    file.write("month height tower a b c tilt tiltaz W_f_1 W_f_2 W_f_3\n")
+    start_date = datetime.datetime(2022, 11, 1)
+    end_date = datetime.datetime(2023, 6, 19)
+    datelist = [start_date + datetime.timedelta(days=x) for x in range((end_date-start_date).days + 1)]
+    n_weeks = int(np.ceil( len(datelist) / 7 ))
+    file.write("start_date end_date height tower a b c tilt tiltaz W_f_1 W_f_2 W_f_3\n")
 
-    for month,year in [
-        (11,2022),
-        (12,2022),
-        (1,2023),
-        (2,2023),
-        (3,2023),
-        (4,2023),
-        (5,2023),
-        (6,2023),
-    ]:
-        print(f"Processing month: {month}/{year}")
+    for nth_week in range(0,n_weeks):
+        # if we are at the last week (which may be shorter than 7 days)
+        if nth_week == n_weeks - 1:
+            this_weeks_dates = datelist[
+                nth_week*7  :
+            ]
+        else: 
+            this_weeks_dates = datelist[
+                nth_week*7  :
+                (nth_week+1)*7
+            ]
+        this_weeks_startdate = this_weeks_dates[0]
+        this_weeks_enddate = this_weeks_dates[-1]
+        print(f"Processing week starting {this_weeks_startdate.strftime('%Y%m%d')}, ending {this_weeks_enddate.strftime('%Y%m%d')}")
         print(f"=================================")
-        num_days = calendar.monthrange(year, month)[1]
-        datelist = [datetime.date(year, month, day).strftime(DATE_FORMAT_STR) for day in range(1, num_days+1)]
         print("Opening data")
         datasets = []
-        for date in datelist:
+        for date in this_weeks_dates:
             try: 
                 ds = xr.open_dataset(os.path.join(
                     sos_files_dir,
-                    f"isfs_sos_qc_geo_tiltcor_5min_{date}.nc"
+                    f"isfs_sos_qc_geo_tiltcor_5min_{date.strftime('%Y%m%d')}.nc"
                 ))
                 # this ensures we don't access variables that aren't in this dataset, which would throw an error
                 ds_new = ds[set(ds.data_vars).intersection(VARIABLE_NAMES)]
@@ -84,4 +89,4 @@ with open(OUTPUT_FILE, "w") as file:
                 (a,b,c), (tilt, tiltaz), W_f = extrautils.calculate_planar_fit(sos_ds[u_VAR], sos_ds[v_VAR], sos_ds[w_VAR])
                 (u_streamwise, v_streamwise, w_streamwise) = extrautils.apply_planar_fit(sos_ds[u_VAR], sos_ds[v_VAR], sos_ds[w_VAR], a, W_f)
                 [height, tower] = u_VAR[2:].split("m_")
-                file.write(f"{month} {height} {tower} {a} {b} {c} {np.rad2deg(tilt)} {np.rad2deg(tiltaz)} {W_f[0]} {W_f[1]} {W_f[2]}\n")
+                file.write(f"{this_weeks_startdate.strftime('%Y%m%d')} {this_weeks_enddate.strftime('%Y%m%d')} {height} {tower} {a} {b} {c} {np.rad2deg(tilt)} {np.rad2deg(tiltaz)} {W_f[0]} {W_f[1]} {W_f[2]}\n")
