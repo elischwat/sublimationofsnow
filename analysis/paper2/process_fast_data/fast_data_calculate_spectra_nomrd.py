a00de554780560292b34f16b56cff942ec1f07cc
elischwat <eli.l.schwat@gmail.com>
2025-06-13 12:17:22 -0700

    update script to run on server

--- analysis/paper2/process_fast_data/fast_data_calculate_spectra_nomrd.py
+++ analysis/paper2/process_fast_data/fast_data_calculate_spectra_nomrd.py
@@ -9,36 +9,36 @@
     #######################################
     python analysis/paper2/process_fast_data/fast_data_calculate_spectra_nomrd.py \
         -f \
-            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast/isfs_sos_qc_geo_tiltcor_hr_20230409_21.nc' \
-            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast/isfs_sos_qc_geo_tiltcor_hr_20230409_22.nc' \
-            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast/isfs_sos_qc_geo_tiltcor_hr_20230409_23.nc' \
+            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast_old/isfs_sos_qc_geo_tiltcor_hr_20230409_21.nc' \
+            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast_old/isfs_sos_qc_geo_tiltcor_hr_20230409_22.nc' \
+            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast_old/isfs_sos_qc_geo_tiltcor_hr_20230409_23.nc' \
         -o "/Users/elischwat/Development/data/sublimationofsnow/mrd/NOmrds/" \
         -s 1000 \
         -p 16
 
     python analysis/paper2/process_fast_data/fast_data_calculate_spectra_nomrd.py \
         -f \
-            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast/isfs_sos_qc_geo_tiltcor_hr_20230418_12.nc' \
-            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast/isfs_sos_qc_geo_tiltcor_hr_20230418_13.nc' \
-            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast/isfs_sos_qc_geo_tiltcor_hr_20230418_14.nc' \
+            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast_old/isfs_sos_qc_geo_tiltcor_hr_20230418_12.nc' \
+            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast_old/isfs_sos_qc_geo_tiltcor_hr_20230418_13.nc' \
+            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast_old/isfs_sos_qc_geo_tiltcor_hr_20230418_14.nc' \
         -o "/Users/elischwat/Development/data/sublimationofsnow/mrd/NOmrds/" \
         -s 1000 \
         -p 16
         
     python analysis/paper2/process_fast_data/fast_data_calculate_spectra_nomrd.py \
         -f \
-            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast/isfs_sos_qc_geo_tiltcor_hr_20230418_21.nc' \
-            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast/isfs_sos_qc_geo_tiltcor_hr_20230418_22.nc' \
-            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast/isfs_sos_qc_geo_tiltcor_hr_20230418_23.nc' \
+            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast_old/isfs_sos_qc_geo_tiltcor_hr_20230418_21.nc' \
+            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast_old/isfs_sos_qc_geo_tiltcor_hr_20230418_22.nc' \
+            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast_old/isfs_sos_qc_geo_tiltcor_hr_20230418_23.nc' \
         -o "/Users/elischwat/Development/data/sublimationofsnow/mrd/NOmrds/" \
         -s 1000 \
         -p 16
 
     python analysis/paper2/process_fast_data/fast_data_calculate_spectra_nomrd.py \
         -f \
-            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast/isfs_sos_qc_geo_tiltcor_hr_20221221_21.nc' \
-            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast/isfs_sos_qc_geo_tiltcor_hr_20221221_22.nc' \
-            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast/isfs_sos_qc_geo_tiltcor_hr_20221221_23.nc' \
+            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast_old/isfs_sos_qc_geo_tiltcor_hr_20221221_21.nc' \
+            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast_old/isfs_sos_qc_geo_tiltcor_hr_20221221_22.nc' \
+            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast_old/isfs_sos_qc_geo_tiltcor_hr_20221221_23.nc' \
         -o "/Users/elischwat/Development/data/sublimationofsnow/mrd/NOmrds/" \
         -s 1000 \
         -p 16
@@ -46,9 +46,9 @@
 
     python analysis/paper2/process_fast_data/fast_data_calculate_spectra_nomrd.py \
         -f \
-            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast/isfs_sos_qc_geo_tiltcor_hr_20230304_21.nc' \
-            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast/isfs_sos_qc_geo_tiltcor_hr_20230304_22.nc' \
-            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast/isfs_sos_qc_geo_tiltcor_hr_20230304_23.nc' \
+            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast_old/isfs_sos_qc_geo_tiltcor_hr_20230304_21.nc' \
+            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast_old/isfs_sos_qc_geo_tiltcor_hr_20230304_22.nc' \
+            '/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast_old/isfs_sos_qc_geo_tiltcor_hr_20230304_23.nc' \
         -o "/Users/elischwat/Development/data/sublimationofsnow/mrd/NOmrds/" \
         -s 1000 \
         -p 16
@@ -68,8 +68,32 @@ from joblib import Parallel, delayed
 from tqdm import tqdm
 import os
 import math
+from datetime import datetime, timedelta
 
-def fast_data_files_to_dataframe(file_list):
+# First and second rotations that comprise the double rotation,
+# as described here: http://link.springer.com/10.1023/A:1018966204465
+def apply_first_rotation(df, u_var, v_var, w_var):
+    mean_u = df[u_var].mean()
+    mean_v = df[v_var].mean()
+    theta = np.arctan2(mean_v, mean_u)
+    adj_u = df[u_var]*np.cos(theta) + df[v_var]*np.sin(theta)
+    adj_v = -df[u_var]*np.sin(theta) + df[v_var]*np.cos(theta)
+    df[u_var] = adj_u
+    df[v_var] = adj_v
+    return df
+def apply_second_rotation(df, u_var, v_var, w_var):
+    mean_u = df[u_var].mean()
+    mean_w = df[w_var].mean()
+    phi = np.arctan2(mean_w, mean_u)
+    adj_u = df[u_var]*np.cos(phi) + df[w_var]*np.sin(phi)
+    adj_w = - df[u_var]*np.sin(phi) + df[w_var]*np.cos(phi)
+    df[u_var] = adj_u
+    df[w_var] = adj_w
+    return df
+
+
+def fast_data_files_to_dataframe(file_list, rotation = 'double'):
+    assert rotation in ['single', 'double', 'none']
     ## Open files, prep timestamps, combine into dataset
     index_vars = ['base_time']
     value_vars = [
@@ -86,6 +110,7 @@ def fast_data_files_to_dataframe(file_list):
         'u_3m_d',	'v_3m_d',	'w_3m_d',	'h2o_3m_d',     'tc_3m_d',
         'u_10m_d',	'v_10m_d',	'w_10m_d',	'h2o_10m_d',    'tc_10m_d',
     ]
+    suffixes = [ '2m_c',  '3m_c',  '5m_c',  '10m_c',  '15m_c',  '20m_c',  '3m_uw',  '10m_uw',  '3m_ue',  '10m_ue',  '3m_d',  '10m_d']
     VARIABLES = index_vars + value_vars
     fast_ds = xr.open_mfdataset( file_list, concat_dim="time",  combine="nested",  data_vars=VARIABLES)
     fast_df = fast_ds[VARIABLES].to_dataframe()
@@ -100,16 +125,22 @@ def fast_data_files_to_dataframe(file_list):
         axis = 1
     )
     fast_df = utils.modify_df_timezone(fast_df, pytz.UTC, "US/Mountain")
+
+    # Interpolate all the variables 
+    fast_df = fast_df.set_index('time')[value_vars].interpolate().reset_index()
+
+    if rotation != 'none':
+        for suffix in suffixes:
+            # then rotation is single or double, and we at least need to do the first rotation
+            fast_df = apply_first_rotation(fast_df, f'u_{suffix}', f'v_{suffix}', f'w_{suffix}')
+            print("mean u, v, w after first rotation ", fast_df[[f'u_{suffix}', f'v_{suffix}', f'w_{suffix}']].mean())    
+                    
+        if rotation == 'double':
+            for suffix in suffixes:
+                # then rotation is single or double, and we at least need to do the first rotation
+                fast_df = apply_second_rotation(fast_df, f'u_{suffix}', f'v_{suffix}', f'w_{suffix}')
+                print("mean u, v, w after second rotation ", fast_df[[f'u_{suffix}', f'v_{suffix}', f'w_{suffix}']].mean())    
     
-    # Rotate three hours of fast data into the mean wind direction
-    mean_u = fast_df['u_3m_c'].mean()
-    mean_v = fast_df['v_3m_c'].mean()
-    theta = np.arctan2(mean_v, mean_u)
-    adj_u = fast_df['u_3m_c']*np.cos(theta) + fast_df['v_3m_c']*np.sin(theta)
-    adj_v = -fast_df['u_3m_c']*np.sin(theta) + fast_df['v_3m_c']*np.cos(theta)
-    fast_df['u_3m_c'] = adj_u
-    fast_df['v_3m_c'] = adj_v
-    print("mean u and v after rotation ", fast_df[['u_3m_c', 'v_3m_c']].mean())
     return fast_df
 
 ##########
@@ -176,41 +207,8 @@ def calculate_mrd_for_df(df, VAR1, VAR2, shift, parallelism):
 
     return pd.concat(processed_results)
 
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-f",
-        "--input-file-list",
-        nargs="*",
-        required=True,
-        help="List of file paths to process, must be contiguous in time"
-    )
-    parser.add_argument(
-        "-o",
-        "--output-path",
-        type=str,
-        required=True,
-        help="Output directory for files with calculated spectra."
-    )
-    parser.add_argument(
-        "-s",
-        "--shift",
-        type=int,
-        required=True,
-        help="Shift for overlapping data segments on which MRDs are performed."
-    )
-    parser.add_argument(
-        "-p",
-        "--parallelism",
-        type=int,
-        required=True,
-        help="Number of cores to use for parallel processing."
-    )
-    args = parser.parse_args()    
-    print(args.input_file_list)
-    
-    fast_df = fast_data_files_to_dataframe(args.input_file_list)
+def process_args(input_file_list, shift, parallelism, output_path):
+    fast_df = fast_data_files_to_dataframe(input_file_list)
     print(f"Number of valid rows pre-forward-filling: {len(fast_df.dropna())}")
     fast_df = fast_df.ffill()
     print(f"Number of valid rows post-forward-filling: {len(fast_df.dropna())}")
@@ -238,13 +236,78 @@ if __name__ == '__main__':
     spectra_results_list = []
     for prefix1, prefix2 in variable_prefixes_for_spectra_calculation:
         for suffix in suffixes:
-            spectra_result = calculate_mrd_for_df(fast_df, prefix1 + suffix, prefix2 + suffix, shift = args.shift, parallelism=args.parallelism)
+            spectra_result = calculate_mrd_for_df(fast_df, prefix1 + suffix, prefix2 + suffix, shift = shift, parallelism = parallelism)
             spectra_result['covariance'] = prefix1 + prefix2
             spectra_result['loc'] = suffix
             spectra_results_list.append(spectra_result)
     output_file = os.path.join(
-            args.output_path, 
+            output_path, 
             f"{fast_df.time.min()}_{fast_df.time.max()}.parquet"
         )
     print(f"Saving to output_file: {output_file}")
     pd.concat(spectra_results_list).to_parquet(output_file)
+
+if __name__ == '__main__':
+    ####################################
+    # THIS VERSION READS FROM TERMINAL
+    ####################################
+
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument(
+    #     "-f",
+    #     "--input-file-list",
+    #     nargs="*",
+    #     required=True,
+    #     help="List of file paths to process, must be contiguous in time"
+    # )
+    # parser.add_argument(
+    #     "-o",
+    #     "--output-path",
+    #     type=str,
+    #     required=True,
+    #     help="Output directory for files with calculated spectra."
+    # )
+    # parser.add_argument(
+    #     "-s",
+    #     "--shift",
+    #     type=int,
+    #     required=True,
+    #     help="Shift for overlapping data segments on which MRDs are performed."
+    # )
+    # parser.add_argument(
+    #     "-p",
+    #     "--parallelism",
+    #     type=int,
+    #     required=True,
+    #     help="Number of cores to use for parallel processing."
+    # )
+    # args = parser.parse_args()    
+    # process_args(args.input_file_list, args.shift, args.parallelism, args.output_path)
+    
+
+    ####################################
+    # THIS VERSION RUNS FROM THE SCRIPT
+    ####################################
+
+    # Define the start and end dates
+    start_date = datetime(2023, 3, 1)
+    end_date = datetime(2023, 5, 1)
+
+    # Generate the list of date strings
+    date_list = []
+    current_date = start_date
+    while current_date < end_date:
+        date_list.append(current_date.strftime('%Y%m%d'))
+        current_date += timedelta(days=1)
+    for date in date_list:
+        print(f"Processing date: {date}")
+
+        input_file_list = [
+            f'/storage/elilouis/sublimationofsnow/sosqc_fast/isfs_sos_qc_geo_tiltcor_hr_{date}_21.nc'
+            f'/storage/elilouis/sublimationofsnow/sosqc_fast/isfs_sos_qc_geo_tiltcor_hr_{date}_22.nc'
+            f'/storage/elilouis/sublimationofsnow/sosqc_fast/isfs_sos_qc_geo_tiltcor_hr_{date}_23.nc'
+        ]
+        shift = 2000
+        parallelism = 20
+        output_path = "/storage/elilouis/sublimationofsnow/mrd/NOmrds/"
+        process_args(input_file_list, shift, parallelism, output_path)
