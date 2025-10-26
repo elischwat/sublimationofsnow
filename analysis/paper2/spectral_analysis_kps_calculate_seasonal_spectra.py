bb8f4d2e9a62a178ca9efaa598e62f595bb1e12c
elischwat <eli.l.schwat@gmail.com>
2025-10-26 13:03:22 -0700

    modifying scripts to speed up mrd calculations

--- analysis/paper2/spectral_analysis_kps_calculate_seasonal_spectra.py
+++ analysis/paper2/spectral_analysis_kps_calculate_seasonal_spectra.py
@@ -4,50 +4,84 @@ import numpy as np
 from process_fast_data.fast_data_calculate_spectra_nomrd import calculate_mrd_for_df, fast_data_files_to_dataframe
 from sublimpy import utils
 import pandas as pd
+from dask.distributed import Client
+
+import time
 
 ### INPUTS
-DATA_DIR = "/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast/"
-OUTPUT_DIR = ""
-START_DATE = '20221201'
-END_DATE = '20230501'
+# DATA_DIR = "/Users/elischwat/Development/data/sublimationofsnow/sosqc_fast/"
+DATA_DIR = "/storage/elilouis/sublimationofsnow/sosqc_fast"
+OUTPUT_DIR = "mrds_uw"
+START_DATE = '20221130'
+END_DATE = '20230615'
+VAR1='u'
+VAR2='w'
 
-### PREP LIST OF DATES
-dates = pd.date_range(START_DATE, END_DATE, freq='D')
-date_pairs = list(zip(
-    dates[:-1],
-    dates[1:]
-))
+def do_the_work():
+    ### PREP LIST OF DATES
+    dates = pd.date_range(START_DATE, END_DATE, freq='D')
+    date_pairs = list(zip(
+        dates[:-1],
+        dates[1:]
+    ))
 
-for d1, d2 in date_pairs:
-    print(f'Processing {d1}')
-    dates_subset = [d1, d2]
-    files = sorted(np.array([
-        glob.glob(os.path.join(
-            DATA_DIR, 
-            f"isfs_sos_qc_geo_tiltcor_hr_v2_{d.strftime('%Y%m%d')}**.nc"
-        )) for d in dates_subset
-    ]).flatten())[6:6+25]
-    fast_df_sos_all_data = fast_data_files_to_dataframe(
-        files,
-        rotation='none'
-    )
-    fast_df_sos_all_data = utils.modify_df_timezone(fast_df_sos_all_data, 'UTC', 'US/Mountain')
-    fast_df_sos_all_data = fast_df_sos_all_data.set_index('time').loc[d1.strftime('%Y%m%d')]
-    fast_df_sos = fast_df_sos_all_data[['u_3m_c', 'v_3m_c', 'w_3m_c']].rename(columns={
-        'u_3m_c': 'u',
-        'v_3m_c': 'v',
-        'w_3m_c': 'w',
-    })
-    fast_df_sos = fast_df_sos.resample('0.1s').mean()
-    fast_df_sos.index = fast_df_sos.index.get_level_values(1)
-    fast_df_sos['u'] = fast_df_sos['u'].interpolate()
-    fast_df_sos['v'] = fast_df_sos['v'].interpolate()
-    fast_df_sos['w'] = fast_df_sos['w'].interpolate()
-    mrd_uw_sos = calculate_mrd_for_df(
-        fast_df_sos[['u', 'v', 'w']].reset_index(), 
-        'u', 'v', 
-        shift=6000, # 10 minute sliding window
-        parallelism=20, 
-        M=14, # 27.31 minute long calculations
-    )
-    mrd_uw_sos.to_parquet(os.path.join(OUTPUT_DIR, d1.strftime('%Y%m%d') + '.csv'))
+    for d1, d2 in date_pairs:
+        print(f'Processing {d1}')
+        # Get the current time (first time)
+        start_time = time.time()
+        
+        dates_subset = [d1, d2]
+        files = sorted(np.array([
+            glob.glob(os.path.join(
+                DATA_DIR, 
+                f"isfs_sos_qc_geo_tiltcor_hr_v2_{d.strftime('%Y%m%d')}**.nc"
+            )) for d in dates_subset
+        ]).flatten())[6:6+25]
+        print(f'...opening files')
+        
+        fast_df_sos_all_data = fast_data_files_to_dataframe(
+            files,
+            rotation='none'
+        )
+        fast_df_sos_all_data = utils.modify_df_timezone(fast_df_sos_all_data, 'UTC', 'US/Mountain')
+        fast_df_sos_all_data = fast_df_sos_all_data.set_index('time').loc[d1.strftime('%Y%m%d')]
+        fast_df_sos = fast_df_sos_all_data[['u_3m_c', 'v_3m_c', 'w_3m_c']].rename(columns={
+            'u_3m_c': 'u',
+            'v_3m_c': 'v',
+            'w_3m_c': 'w',
+        })
+        print(f'......Elapsed time: {time.time() - start_time} seconds')
+        print(f'...resampling')
+        
+        fast_df_sos = fast_df_sos.resample('0.1S').mean()
+        # fast_df_sos.index = fast_df_sos.index.get_level_values(1)
+        print(f'......Elapsed time: {time.time() - start_time} seconds')
+        print(f'...interpolating')
+        
+        fast_df_sos = fast_df_sos.assign(
+            u = fast_df_sos['u'].interpolate(),
+            v = fast_df_sos['v'].interpolate(),
+            w = fast_df_sos['w'].interpolate(),
+        )
+        print(f'......Elapsed time: {time.time() - start_time} seconds')
+        print(f'...running mrds')
+        
+        mrd_uw_sos = calculate_mrd_for_df(
+            fast_df_sos[['u', 'v', 'w']].reset_index(), 
+            VAR1, VAR2,
+            shift=6000, # 10 minute sliding window
+            parallelism=22, 
+            M=14, # 27.31 minute long calculations,
+            double_rotate=True
+        )
+        print(f'......Elapsed time: {time.time() - start_time} seconds')
+        print(f'...saving file')
+        
+        mrd_uw_sos.to_parquet(os.path.join(OUTPUT_DIR, d1.strftime('%Y%m%d') + '.csv'))
+        print(f'......Elapsed time: {time.time() - start_time} seconds')
+        print(f'...file saved, iteration finished')
+        print()
+        
+if __name__ == '__main__':
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+    _ = do_the_work()
